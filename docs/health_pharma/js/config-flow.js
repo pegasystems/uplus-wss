8581e3737c0c393695bfa8e6f5c0d6c8651d6d7e
marsr <richard.marsot@pega.com>
2019-09-24 16:38:05 -0400

    add U+ health pharma and care

---
+++ docs/health_pharma/js/config-flow.js
@@ -0,0 +1,284 @@
+window.app = {
+  industry: 'health-pharma',
+  herotext: {
+    title: 'hero_text',
+    titlespan: 'hero_span',
+    buttonlabel: 'learnmore',
+  },
+  secondarydetails: [
+    {
+      className: 'contact-us',
+      title: 'secondary1_title',
+      message: 'secondary1_msg',
+      link: 'learnmore',
+    },
+    {
+      className: 'paperless',
+      title: 'secondary2_title',
+      message: 'secondary2_msg',
+      link: 'explorearticles',
+    },
+  ],
+  primarydetails: [
+    {
+      img: 'img/option-1.jpg',
+      title: 'primary1_title',
+      message: 'primary1_msg',
+      link: 'findsupport',
+    },
+    {
+      img: 'img/option-2.jpg',
+      title: 'primary2_title',
+      message: 'primary2_msg',
+      link: 'findsavings',
+    },
+    {
+      img: 'img/option-3.jpg',
+      title: 'primary3_title',
+      message: 'primary3_msg',
+      link: 'learnmore',
+    },
+  ],
+  accountpicker: [
+    {
+      icon: 'pi-stethoscope',
+      title: 'accountpicker1_title',
+      desc: 'accountpicker1_desc',
+      id: '',
+    },
+    {
+      icon: 'pi-case-medical',
+      title: 'accountpicker2_title',
+      desc: 'accountpicker2_desc',
+      id: '',
+    },
+    {
+      icon: 'pi-shield',
+      title: 'accountpicker3_title',
+      desc: 'accountpicker3_desc',
+      id: '',
+    },
+  ],
+  recentactivity: [
+    {
+      message: 'recentactivity1_msg',
+    },
+    {
+      message: 'recentactivity2_msg',
+    },
+    {
+      message: 'recentactivity3_msg',
+    },
+    {
+      message: 'recentactivity4_msg',
+    },
+  ],
+  offers: [
+    {
+      img: 'offer-image.jpg',
+      title: 'offer1_title',
+      message: 'offer1_msg',
+    },
+  ],
+  menuitems: [
+    {
+      title: 'menu1',
+    },
+    {
+      title: 'menu2',
+    },
+    {
+      title: 'menu3',
+    },
+    {
+      title: 'menu4',
+    },
+  ],
+  landing: {
+    header: {
+      title: 'landing_header_title',
+      promotions: [
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion1',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion2',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion3',
+        },
+      ],
+    },
+    main_form: {
+      title: 'landing_main_form_title',
+      button_label: 'landing_main_form_button_label',
+      data: [
+        {
+          label: 'landing_main_form_data1_label',
+        },
+        {
+          label: 'landing_main_form_data2_label',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          label: 'landing_main_form_data3_label',
+        },
+      ],
+    },
+    cards: {
+      title: 'landing_cards_title',
+      data: [
+        {
+          icon: 'pi-chevron-solid',
+          title: 'landing_cards_data1_title',
+          message: 'landing_cards_data1_message',
+        },
+        {
+          icon: 'pi-triangle-solid',
+          title: 'landing_cards_data2_title',
+          message: 'landing_cards_data2_message',
+        },
+        {
+          icon: 'pi-bolt-solid',
+          title: 'landing_cards_data3_title',
+          message: 'landing_cards_data3_message',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'landing_details1_title',
+        message: 'landing_details1_message',
+        image: 'landing-detail-1.png',
+      },
+      {
+        title: 'landing_details2_title',
+        message: 'landing_details2_message',
+        image: 'landing-detail-2.png',
+      },
+    ],
+    benefits: {
+      title: 'landing_benefits_title',
+      data: [
+        {
+          icon: 'pi-passbook',
+          message: 'landing_benefits_data1_message',
+        },
+        {
+          icon: 'pi-information-solid',
+          message: 'landing_benefits_data2_message',
+        },
+        {
+          icon: 'pi-receipt',
+          message: 'landing_benefits_data3_message',
+        },
+        {
+          icon: 'pi-bolt',
+          message: 'landing_benefits_data4_message',
+        },
+        {
+          icon: 'pi-disc-stack',
+          message: 'landing_benefits_data5_message',
+        },
+        {
+          icon: 'pi-bolt',
+          message: 'landing_benefits_data6_message',
+        },
+      ],
+    },
+    footer: {
+      title: 'landing_footer_title',
+      button_label: 'landing_footer_button_label',
+    },
+  },
+  offer: {
+    header: {
+      title: 'offer_header_title',
+      msg: 'offer_header_msg',
+    },
+    main_offer: {
+      title: 'offer_main_offer_title',
+      message: 'offer_main_offer_msg',
+      button_label: 'offer_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer_cards_title',
+      data: [
+        {
+          title: 'offer_cards_data1_title',
+          message: 'offer_cards_data1_msg',
+          highlight: 'offer_cards_data1_highlight',
+        },
+        {
+          title: 'offer_cards_data2_title',
+          message: 'offer_cards_data2_msg',
+          highlight: 'offer_cards_data2_highlight',
+        },
+        {
+          title: 'offer_cards_data3_title',
+          message: 'offer_cards_data3_msg',
+          highlight: 'offer_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer_details1_title',
+        message: 'offer_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer_highlights_data1_title',
+          message: 'offer_highlights_data1_msg',
+        },
+        {
+          icon: 'pi-book-open',
+          title: 'offer_highlights_data2_title',
+          message: 'offer_highlights_data2_msg',
+        },
+        {
+          icon: 'pi-compass',
+          title: 'offer_highlights_data3_title',
+          message: 'offer_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer_footer_title',
+      button_label: 'offer_footer_button_label',
+    },
+  },
+  phone: {
+    home_page: {
+      title: 'phone_home_page_title',
+      promo: {
+        image: 'option-2.jpg',
+      },
+      actions: [
+        {
+          icon: 'pi-calculator',
+          page: 'account',
+          title: 'phone_home_page_actions1_title',
+        },
+        {
+          icon: 'pi-document-data',
+          page: 'quicklinks1',
+          title: 'phone_home_page_actions2_title',
+        },
+        {
+          icon: 'pi-dollar-sign',
+          page: 'home',
+          title: 'phone_home_page_actions3_title',
+        },
+      ],
+    },
+  },
+};
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.5e47002d9d86dd593ee2a5e33fc8ed21.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.17f00c65634bc767587f3125ce071b25.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox self */
 /* eslint no-restricted-globals: 0  */
