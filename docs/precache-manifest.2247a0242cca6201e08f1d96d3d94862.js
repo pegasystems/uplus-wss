1aab827af651a47a8c79270ba9f323dfffa90f21
marsr <richard.marsot@pega.com>
2020-03-23 11:52:00 -0400

    fix footer button and lang file for insurance

--- docs/precache-manifest.2247a0242cca6201e08f1d96d3d94862.js
+++ docs/precache-manifest.2247a0242cca6201e08f1d96d3d94862.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "./js/auto.e17b939b.js"
   },
   {
-    "revision": ".30e8bc3137a193539a",
+    "revision": ".e5936262b5e748a4bf",
     "url": "./css/chunk-common.71a5f61d.css"
   },
   {
-    "revision": ".30e8bc3137a193539a",
-    "url": "./js/chunk-common.5c24252e.js"
+    "revision": ".e5936262b5e748a4bf",
+    "url": "./js/chunk-common.aa8bb77f.js"
   },
   {
     "revision": ".79c91b5869c1256ed3",
@@ -64,6 +64,10 @@ self.__precacheManifest = [
     "url": "./fonts/uikit-icons.5fdbff85.woff"
   },
   {
+    "revision": ".b3f3e21fae06fae9dd11b9f65b173d",
+    "url": "./img/checkmark-w.71b3f3e2.svg"
+  },
+  {
     "revision": ".c6b63072760f2685267ff8f787787d",
     "url": "./img/paperless.3cc6b630.svg"
   },
@@ -72,76 +76,56 @@ self.__precacheManifest = [
     "url": "./img/contact.e5a3bcc3.svg"
   },
   {
-    "revision": ".b3f3e21fae06fae9dd11b9f65b173d",
-    "url": "./img/checkmark-w.71b3f3e2.svg"
-  },
-  {
-    "revision": ".b28ebbb8a2ff12902bb76c82765542",
+    "revision": ".d5923e35b62bff5cbb93492207fe8f",
     "url": "./commercial_bank/index.html"
   },
   {
-    "revision": ".e3197c6a5c5588cf1e03ef4f2bbe5a",
+    "revision": ".74351a01cc0a46f9c905c6508e22af",
     "url": "./retail_bank/index.html"
   },
   {
-    "revision": ".2fcb1b424388714933c0b026b2c65c",
+    "revision": ".4efa4be6d8e6fc5795653a60528a0d",
     "url": "./comms/index.html"
   },
   {
-    "revision": ".f52ef58770951bb7e0c96243d10447",
+    "revision": ".4f186fd9214d4180cdb0ad479f4887",
     "url": "./health_payer/index.html"
   },
   {
-    "revision": ".fbea88a7f905b2da58a50fd80b3555",
+    "revision": ".d56c0884962cfe72efaf2de0b271c4",
     "url": "./health_provider/index.html"
   },
   {
-    "revision": ".cf69cf1e81323d3d93469508b890c7",
+    "revision": ".98b9c6bbae858c215f34616511a978",
     "url": "./health_care/index.html"
   },
   {
-    "revision": ".f03d683fee0ab79c248bea05ae643b",
+    "revision": ".80c72aeeeb10263773a001c08251b5",
     "url": "./health_pharma/index.html"
   },
   {
-    "revision": ".4463216b0a2b37c19b55d1b43805c0",
+    "revision": ".242df9f0ee62f1c7075d5353457729",
     "url": "./gov/index.html"
   },
   {
-    "revision": ".e933edc6d29d2349af0beaf6bfb50f",
+    "revision": ".5ed44391f6198db4caf7154240b66c",
     "url": "./insurance/index.html"
   },
   {
-    "revision": ".926603ab4089cc9f524f1d355dd9a4",
+    "revision": ".3dd753daa532815608cc7962102b55",
     "url": "./manufacturing/index.html"
   },
   {
-    "revision": ".50caf4031780a9b4d9a384a9797c25",
+    "revision": ".fbea6d5248a055c208b01808ffa4d1",
     "url": "./auto/index.html"
-  },
-  {
-    "revision": ".37e4b4c28aaf167f1b9976e97dc2b3",
-    "url": "./auto/css/override.css"
-  },
-  {
-    "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
-    "url": "./auto/form-embed.html"
-  },
-  {
-    "revision": ".9e834a88309e07f0e243eefaab531e",
-    "url": "./auto/i18n/lang-en.js"
-  },
-  {
-    "revision": ".69d9db1419014cd82ec4f12b729d7a",
-    "url": "./auto/img/u+-logo.svg"
   },
   {
     "revision": ".ee34a16a17c4f7ccbb94da77b76541",
     "url": "./404.html"
   },
   {
-    "revision": ".2bba4a7f49386209cfe6e9df6c2c2f",
-    "url": "./auto/js/config-settings.js"
+    "revision": ".37e4b4c28aaf167f1b9976e97dc2b3",
+    "url": "./auto/css/override.css"
   },
   {
     "revision": ".ef4293fdbacc540447780a2acd8ace",
@@ -152,16 +136,32 @@ self.__precacheManifest = [
     "url": "./commercial_bank/charts/balance.html"
   },
   {
-    "revision": ".0d8844c3ba96659bfce005b293acaa",
-    "url": "./commercial_bank/i18n/lang-en.js"
+    "revision": ".69d9db1419014cd82ec4f12b729d7a",
+    "url": "./auto/img/u+-logo.svg"
+  },
+  {
+    "revision": ".2bba4a7f49386209cfe6e9df6c2c2f",
+    "url": "./auto/js/config-settings.js"
+  },
+  {
+    "revision": ".dd4cb0fb26045afb6b9f53a736c933",
+    "url": "./commercial_bank/charts/data.csv"
   },
   {
     "revision": ".a9814fe1fb64d431d6c45ed626eca8",
     "url": "./commercial_bank/css/override.css"
   },
   {
-    "revision": ".dd4cb0fb26045afb6b9f53a736c933",
-    "url": "./commercial_bank/charts/data.csv"
+    "revision": ".0d8844c3ba96659bfce005b293acaa",
+    "url": "./commercial_bank/i18n/lang-en.js"
+  },
+  {
+    "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
+    "url": "./auto/form-embed.html"
+  },
+  {
+    "revision": ".9e834a88309e07f0e243eefaab531e",
+    "url": "./auto/i18n/lang-en.js"
   },
   {
     "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
@@ -170,10 +170,6 @@ self.__precacheManifest = [
   {
     "revision": ".d930e6e850492b8527d14ad46f26d1",
     "url": "./commercial_bank/img/u+-logo.svg"
-  },
-  {
-    "revision": ".799546d7290d237e725e95f7e0e6a6",
-    "url": "./commercial_bank/js/config-settings.js"
   },
   {
     "revision": ".e66405fb8e4c7751a4dec05d3f43ad",
@@ -192,12 +188,24 @@ self.__precacheManifest = [
     "url": "./comms/i18n/lang-en.js"
   },
   {
+    "revision": ".799546d7290d237e725e95f7e0e6a6",
+    "url": "./commercial_bank/js/config-settings.js"
+  },
+  {
     "revision": ".1add469672c5799c3a2f0adbadf637",
     "url": "./comms/img/u+-logo.svg"
   },
   {
+    "revision": ".c071bf9cd85d912d8360cf53ddc931",
+    "url": "./comms/js/config-flow.js"
+  },
+  {
     "revision": ".d0234dc6bb1b3b52c71de1355c7d8e",
     "url": "./comms/js/config-settings.js"
+  },
+  {
+    "revision": ".b994fd93882918b4f01909d8e20808",
+    "url": "./favicon.ico"
   },
   {
     "revision": ".026780baa27aefb21f08b747155163",
@@ -208,20 +216,24 @@ self.__precacheManifest = [
     "url": "./gov/form-embed.html"
   },
   {
-    "revision": ".c071bf9cd85d912d8360cf53ddc931",
-    "url": "./comms/js/config-flow.js"
-  },
-  {
     "revision": ".884c1972282fb67af1fb6760b1d11e",
     "url": "./gov/i18n/lang-en.js"
+  },
+  {
+    "revision": ".abe76efb5e9af39e16c89bdd4b0564",
+    "url": "./gov/img/u+-logo.svg"
+  },
+  {
+    "revision": ".2c260ef412ce953ef86c1b0d59c4dc",
+    "url": "./health_care/css/override.css"
   },
   {
     "revision": ".437163f690c383bac76a270903c094",
     "url": "./gov/js/config-settings.js"
   },
   {
-    "revision": ".abe76efb5e9af39e16c89bdd4b0564",
-    "url": "./gov/img/u+-logo.svg"
+    "revision": ".23190d7e6dfa09f2497c90246afe07",
+    "url": "./gov/js/config-flow.js"
   },
   {
     "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
@@ -232,18 +244,6 @@ self.__precacheManifest = [
     "url": "./health_care/i18n/lang-en.js"
   },
   {
-    "revision": ".2c260ef412ce953ef86c1b0d59c4dc",
-    "url": "./health_care/css/override.css"
-  },
-  {
-    "revision": ".b994fd93882918b4f01909d8e20808",
-    "url": "./favicon.ico"
-  },
-  {
-    "revision": ".23190d7e6dfa09f2497c90246afe07",
-    "url": "./gov/js/config-flow.js"
-  },
-  {
     "revision": ".bb413658287d9bcdb8789df76eaa40",
     "url": "./health_care/img/u+-logo.svg"
   },
@@ -252,16 +252,32 @@ self.__precacheManifest = [
     "url": "./health_payer/form-embed.html"
   },
   {
+    "revision": ".829a71a8941a821fe6f726b52ba8de",
+    "url": "./health_care/js/config-settings.js"
+  },
+  {
     "revision": ".2a42834454befdd48724a5da3fb927",
     "url": "./health_payer/css/override.css"
+  },
+  {
+    "revision": ".0cda46ed4cc8caea40083c0a1bc6b3",
+    "url": "./health_payer/i18n/lang-en.js"
   },
   {
     "revision": ".8200a3770887df06730f7b4bc62070",
     "url": "./health_care/js/config-flow.js"
   },
   {
-    "revision": ".0cda46ed4cc8caea40083c0a1bc6b3",
-    "url": "./health_payer/i18n/lang-en.js"
+    "revision": ".42d6c79d17b48412919cc15f84c46f",
+    "url": "./health_payer/js/config-flow.js"
+  },
+  {
+    "revision": ".0905fb0f3d76a36fd3da8031ecf3c1",
+    "url": "./health_payer/js/config-settings.js"
+  },
+  {
+    "revision": ".c7044390bc891e1117fbb10ddfdabe",
+    "url": "./health_pharma/i18n/lang-en.js"
   },
   {
     "revision": ".bb413658287d9bcdb8789df76eaa40",
@@ -272,28 +288,8 @@ self.__precacheManifest = [
     "url": "./health_pharma/css/override.css"
   },
   {
-    "revision": ".0905fb0f3d76a36fd3da8031ecf3c1",
-    "url": "./health_payer/js/config-settings.js"
-  },
-  {
-    "revision": ".42d6c79d17b48412919cc15f84c46f",
-    "url": "./health_payer/js/config-flow.js"
-  },
-  {
-    "revision": ".c7044390bc891e1117fbb10ddfdabe",
-    "url": "./health_pharma/i18n/lang-en.js"
-  },
-  {
     "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
     "url": "./health_pharma/form-embed.html"
-  },
-  {
-    "revision": ".829a71a8941a821fe6f726b52ba8de",
-    "url": "./health_care/js/config-settings.js"
-  },
-  {
-    "revision": ".1fb448744330a692d78b9b4ce06265",
-    "url": "./health_pharma/img/u+-logo.svg"
   },
   {
     "revision": ".f6a433f92a1738392d4d84fd87d62e",
@@ -308,28 +304,36 @@ self.__precacheManifest = [
     "url": "./health_pharma/js/config-settings.js"
   },
   {
-    "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
-    "url": "./health_provider/form-embed.html"
-  },
-  {
     "revision": ".9d33343f50a2f163c9fc55465b4331",
     "url": "./health_provider/i18n/lang-en.js"
   },
   {
-    "revision": ".33336ddcfc68d548fbabe44d1d05e4",
-    "url": "./health_provider/js/config-settings.js"
-  },
-  {
-    "revision": ".bb413658287d9bcdb8789df76eaa40",
-    "url": "./health_provider/img/u+-logo.svg"
+    "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
+    "url": "./health_provider/form-embed.html"
   },
   {
     "revision": ".8f056e090a115111bbb4f297808b21",
     "url": "./health_provider/js/config-flow.js"
   },
   {
+    "revision": ".bb413658287d9bcdb8789df76eaa40",
+    "url": "./health_provider/img/u+-logo.svg"
+  },
+  {
+    "revision": ".33336ddcfc68d548fbabe44d1d05e4",
+    "url": "./health_provider/js/config-settings.js"
+  },
+  {
     "revision": ".63f620529651e8100b227cd232a945",
     "url": "./img/ChatbotIcon.svg"
+  },
+  {
+    "revision": ".63960446323f48fa4a6a78801cf073",
+    "url": "./insurance/css/override.css"
+  },
+  {
+    "revision": ".1fb448744330a692d78b9b4ce06265",
+    "url": "./health_pharma/img/u+-logo.svg"
   },
   {
     "revision": ".7104dfbe119d0e1d64983b04e13cbf",
@@ -340,11 +344,7 @@ self.__precacheManifest = [
     "url": "./insurance/form-embed.html"
   },
   {
-    "revision": ".63960446323f48fa4a6a78801cf073",
-    "url": "./insurance/css/override.css"
-  },
-  {
-    "revision": ".75ad3efeb3f93211d12aa750de0bb3",
+    "revision": ".577350668d5ae24ac7486c2737147b",
     "url": "./insurance/i18n/lang-en.js"
   },
   {
@@ -352,40 +352,44 @@ self.__precacheManifest = [
     "url": "./insurance/img/u+-logo.svg"
   },
   {
-    "revision": ".cb6d50ae5341bf017127b42e0f8d41",
-    "url": "./insurance/js/config-settings.js"
+    "revision": ".2af8d986a01034fb08f03d5f66dbe8",
+    "url": "./insurance/js/config-flow.js"
   },
   {
-    "revision": ".a540118bc47ee74fd9a66c28763ac4",
-    "url": "./js/PegaHelperExtension.js"
+    "revision": ".cb6d50ae5341bf017127b42e0f8d41",
+    "url": "./insurance/js/config-settings.js"
   },
   {
     "revision": ".0afd743d9e9643852e31a135a9f3ae",
     "url": "./js/jquery-min.js"
   },
   {
-    "revision": ".d442ef248172003fedc590322f6b22",
-    "url": "./js/realtimecontainerscript.js"
-  },
-  {
     "revision": ".b8ec68a1f19a344bc8211307aaea32",
     "url": "./js/PegaHelper.js"
   },
   {
-    "revision": ".2af8d986a01034fb08f03d5f66dbe8",
-    "url": "./insurance/js/config-flow.js"
+    "revision": ".d442ef248172003fedc590322f6b22",
+    "url": "./js/realtimecontainerscript.js"
+  },
+  {
+    "revision": ".a540118bc47ee74fd9a66c28763ac4",
+    "url": "./js/PegaHelperExtension.js"
+  },
+  {
+    "revision": ".9edd92e0a9ec06e17a365f387dc6b5",
+    "url": "./manifest.json"
   },
   {
     "revision": ".1a5ad2b10c4c5ca12163a30ce6c7df",
     "url": "./manufacturing/form-embed.html"
   },
   {
-    "revision": ".848798efc700ab782e501791400f85",
-    "url": "./manufacturing/i18n/lang-en.js"
+    "revision": ".37e4b4c28aaf167f1b9976e97dc2b3",
+    "url": "./manufacturing/css/override.css"
   },
   {
-    "revision": ".9edd92e0a9ec06e17a365f387dc6b5",
-    "url": "./manifest.json"
+    "revision": ".848798efc700ab782e501791400f85",
+    "url": "./manufacturing/i18n/lang-en.js"
   },
   {
     "revision": ".8c11b784eb3c902a58900c6e68c0c5",
@@ -394,6 +398,10 @@ self.__precacheManifest = [
   {
     "revision": ".ee4cb5eac63ad33030ac43600bd429",
     "url": "./manufacturing/js/config-flow.js"
+  },
+  {
+    "revision": ".8bd80a4f1241717a21cca9c62012f5",
+    "url": "./retail_bank/form-embed.html"
   },
   {
     "revision": ".ab9cbcaa7e62dacd1600b22ac1490f",
@@ -408,16 +416,8 @@ self.__precacheManifest = [
     "url": "./retail_bank/i18n/lang-en.js"
   },
   {
-    "revision": ".8bd80a4f1241717a21cca9c62012f5",
-    "url": "./retail_bank/form-embed.html"
-  },
-  {
     "revision": ".ac211c405b75c774260bc9cfd3d724",
     "url": "./retail_bank/i18n/lang-fr.js"
-  },
-  {
-    "revision": ".37e4b4c28aaf167f1b9976e97dc2b3",
-    "url": "./manufacturing/css/override.css"
   },
   {
     "revision": ".662783b6f8809b2154e8a6d5d76cc0",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.215bcf1ccec8fb9324146fe5ab0438cc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.2247a0242cca6201e08f1d96d3d94862.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox self */
 /* eslint no-restricted-globals: 0  */
